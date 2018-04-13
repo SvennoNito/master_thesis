f47b350db7273e037576fcfc858ac7082b600602
Sven Leach <32299254+SvennoNito@users.noreply.github.com>
2018-04-13 15:11:31 +0200

    Update batch_leakage.sh

--- batch_leakage.sh
+++ batch_leakage.sh
@@ -3,7 +3,7 @@
 #SBATCH --ntasks=48
 #SBATCH --time=0-4
 #SBATCH --mail-type=END,FAIL         
-#SBATCH --mail-user=sven.leach@uol.de 
+#SBATCH --mail-user=xxx@web.de
 module load Python/3.5.2
 hosts=$(srun hostname)
 python -m scoop --hosts $hosts --prolog $(pwd)/load_module.sh -vv EA_cython.py -s -3 -n 1000 -g 100 -c 0 0 1 0 -t 0 10 20 30 40 50 60 70 80 90 100 -p EL gL -f voltage_base steady_state_voltage_stimend
